877a84a858737f60393c35dc404e8e7ef31c84bc
Edward Kmett <ekmett@gmail.com>
2018-08-05 18:54:17 -0400

    ship it, it builds. clearly it is correct (it is not)

--- src/Data/Binary/Succinct.hs
+++ src/Data/Binary/Succinct.hs
@@ -6,3 +6,7 @@ import Data.Binary.Succinct.Get
 class Bitwise a where
   put :: a -> Put
   get :: Get a
+
+instance (Bitwise a, Bitwise b) => Bitwise (a, b) where
+  put = putPair put put
+  get = getPair get get

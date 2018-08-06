c73096fb0eb5d5bf4c6225aa61a2d159f8faf85d
Edward Kmett <ekmett@gmail.com>
2018-08-05 21:54:20 -0400

    generic get/put

--- src/Data/Binary/Succinct/Get.hs
+++ src/Data/Binary/Succinct/Get.hs
@@ -1,22 +1,40 @@
 {-# language DeriveFunctor #-}
+{-# language DefaultSignatures #-}
+{-# language FlexibleContexts #-}
+{-# language EmptyCase #-}
+{-# language TypeOperators #-}
+{-# language LambdaCase #-}
 module Data.Binary.Succinct.Get 
   ( Get(..)
   , getPair
   , get8
+  , Gettable(..)
+  , GGettable(..)
+  , Gettable1(..)
+  , GGettable1(..)
+  , liftGet
   ) where
 
 import Control.Monad (ap)
 import Data.Binary.Succinct.Blob
 import Data.ByteString as Strict
+import Data.Functor.Compose as F
+import Data.Functor.Product as F
+import Data.Functor.Sum as F
+import Data.Int
 import Data.Maybe
+import Data.Proxy
+import qualified Data.Serialize.Get as S
 import Data.Vector.Storable as Storable
 import Data.Word
+import GHC.Generics as G
+
 import HaskellWorks.Data.BalancedParens.RangeMinMax as BP
 import HaskellWorks.Data.BalancedParens.BalancedParens as BP
 import HaskellWorks.Data.RankSelect.Base.Rank1
 import HaskellWorks.Data.RankSelect.Base.Select1
 
-newtype Get a = Get { unGet :: Blob -> Word64 -> a }
+newtype Get a = Get { runGet :: Blob -> Word64 -> a }
   deriving Functor
 
 instance Applicative Get where
@@ -24,7 +42,7 @@ instance Applicative Get where
   (<*>) = ap
 
 instance Monad Get where
-  m >>= k = Get $ \e s -> unGet (k (unGet m e s)) e s
+  m >>= k = Get $ \e s -> runGet (k (runGet m e s)) e s
 
 shapely
   :: (RangeMinMax (Storable.Vector Word64) -> Word64 -> Maybe Word64)
@@ -48,5 +66,127 @@ get8 :: Get Word8
 get8 = Get $ \(Blob meta _ content) i ->
   Strict.index content $ fromIntegral $ rank0' meta i
 
+liftGet :: S.Get a -> Get a
+liftGet g = Get $ \(Blob meta _ content) i ->
+  case S.runGet g $ Strict.drop (fromIntegral $ rank0' meta i) content of
+    Left e -> error e
+    Right a -> a
+
 rank0' :: Rank1 v => v -> Word64 -> Word64 
 rank0' s i = i - rank1 s i
+
+--------------------------------------------------------------------------------
+-- * GGettable
+--------------------------------------------------------------------------------
+
+class Gettable a where
+  get :: Get a
+  default get :: (G.Generic a, GGettable (G.Rep a)) => Get a
+  get = G.to <$> gget 
+
+instance Gettable ()
+instance Gettable Word8 where
+  get = get8
+
+instance Gettable Word16 where get = liftGet S.getWord16le
+instance Gettable Word32 where get = liftGet S.getWord32le
+instance Gettable Word64 where get = liftGet S.getWord64le
+instance Gettable Int8 where get = liftGet S.getInt8
+instance Gettable Int16 where get = liftGet S.getInt16le
+instance Gettable Int32 where get = liftGet S.getInt32le
+instance Gettable Int64 where get = liftGet S.getInt64le
+
+instance Gettable (Proxy a)
+instance Gettable a => Gettable (Maybe a)
+instance Gettable a => Gettable [a]
+instance (Gettable a, Gettable b) => Gettable (a, b)
+instance (Gettable a, Gettable b) => Gettable (Either a b)
+instance Gettable (f (g a)) => Gettable (F.Compose f g a)
+instance (Gettable (f a), Gettable (g a)) => Gettable (F.Product f g a)
+instance (Gettable (f a), Gettable (g a)) => Gettable (F.Sum f g a)
+
+--------------------------------------------------------------------------------
+-- * GGettable
+--------------------------------------------------------------------------------
+
+class GGettable t where
+  gget :: Get (t a)
+
+instance GGettable U1 where
+  gget = pure U1
+
+instance Gettable c => GGettable (K1 i c) where
+  gget = K1 <$> get
+
+instance GGettable f => GGettable (M1 i c f) where
+  gget = M1 <$> gget
+
+instance (GGettable f, GGettable g) => GGettable (f :*: g) where
+  gget = Get $ \d i -> let
+      j = shapely firstChild d i
+      k = shapely nextSibling d j
+    in runGet gget d j :*: runGet gget d k
+
+instance (GGettable f, GGettable g) => GGettable (f :+: g) where
+  gget = Get $ \d i -> case runGet get8 d i of
+    0 -> L1 $ runGet gget d (i+1)
+    1 -> R1 $ runGet gget d (i+1)
+    _ -> error "bad data"
+
+instance (Gettable1 f, GGettable g) => GGettable (f :.: g) where
+  gget = Comp1 <$> get1 gget
+
+--------------------------------------------------------------------------------
+-- * Gettable1
+--------------------------------------------------------------------------------
+
+class Gettable1 f where
+  get1 :: Get a -> Get (f a)
+  default get1 :: (G.Generic1 f, GGettable1 (G.Rep1 f)) => Get a -> Get (f a)
+  get1 f = G.to1 <$> gget1 f
+
+instance Gettable1 Proxy
+instance Gettable1 []
+instance Gettable1 Maybe
+instance Gettable a => Gettable1 (Either a)
+instance Gettable a => Gettable1 ((,) a)
+instance (Functor f, Gettable1 f, Gettable1 g) => Gettable1 (F.Compose f g)
+instance (Gettable1 f, Gettable1 g) => Gettable1 (F.Product f g)
+instance (Gettable1 f, Gettable1 g) => Gettable1 (F.Sum f g)
+
+--------------------------------------------------------------------------------
+-- * GGettable1
+--------------------------------------------------------------------------------
+
+class GGettable1 t where
+  gget1 :: Get a -> Get (t a)
+
+instance GGettable1 U1 where
+  gget1 _ = pure U1
+
+instance Gettable c => GGettable1 (K1 i c) where
+  gget1 _ = K1 <$> get
+
+instance GGettable1 f => GGettable1 (M1 i c f) where
+  gget1 f = M1 <$> gget1 f
+
+instance (GGettable1 f, GGettable1 g) => GGettable1 (f :*: g) where
+  gget1 f = Get $ \d i -> let
+      j = shapely firstChild d i
+      k = shapely nextSibling d j
+    in runGet (gget1 f) d j :*: runGet (gget1 f) d k
+
+instance (GGettable1 f, GGettable1 g) => GGettable1 (f :+: g) where
+  gget1 f = Get $ \d i -> case runGet get8 d i of
+    0 -> L1 $ runGet (gget1 f) d (i+1)
+    1 -> R1 $ runGet (gget1 f) d (i+1)
+    _ -> error "bad data"
+
+instance (Gettable1 f, GGettable1 g) => GGettable1 (f :.: g) where
+  gget1 f = Comp1 <$> get1 (gget1 f)
+
+instance Gettable1 f => GGettable1 (Rec1 f) where
+  gget1 f = Rec1 <$> get1 f
+
+instance GGettable1 Par1 where
+  gget1 f = Par1 <$> f
--- src/Data/Binary/Succinct/Put.hs
+++ src/Data/Binary/Succinct/Put.hs
@@ -1,6 +1,5 @@
 {-# language DeriveFunctor #-}
 {-# language LambdaCase #-}
-{-# language UndecidableInstances #-}
 {-# language DefaultSignatures #-}
 {-# language FlexibleContexts #-}
 {-# language EmptyCase #-}
@@ -20,16 +19,25 @@ module Data.Binary.Succinct.Put {- .Internal -}
   , GPuttable(..)
   , Puttable1(..)
   , GPuttable1(..)
+  , putN
+  , putN_
   ) where
 
-import Control.Monad (ap)
+import Control.Monad (ap, replicateM_)
 import Data.Bits
 import Data.Bits.Coding
 import Data.Bytes.Put
+import Data.ByteString as Strict
 import Data.ByteString.Builder
-import qualified Data.Serialize.Put as Cereal
+import Data.Int
+import Data.Proxy
+import qualified Data.Serialize.Put as S
+import Data.Void
 import Data.Word
 import qualified GHC.Generics as G
+import Data.Functor.Compose as F
+import Data.Functor.Product as F
+import Data.Functor.Sum as F
 
 putLSB :: MonadPut m => Bool -> Coding m ()
 putLSB v = Coding $ \k i b ->
@@ -71,24 +79,24 @@ instance Monad PutM where
 
 -- your job is to properly deal with managing meta, shape and content coherently
 
-meta :: Coding Cereal.PutM a -> PutM a
-meta m = PutM $ \(S o1 d1 o2 d2) -> case Cereal.getPutM (runCoding m go o1 d1) of
+meta :: Coding S.PutM a -> PutM a
+meta m = PutM $ \(S o1 d1 o2 d2) -> case S.getPutM (runCoding m go o1 d1) of
   ((a,o1',d1'), builder) -> Result a (S o1' d1' o2 d2) (W builder mempty mempty)
   where
-    go :: a -> Int -> Word8 -> Cereal.PutM (a, Int, Word8)
+    go :: a -> Int -> Word8 -> S.PutM (a, Int, Word8)
     go a o1' d1' = pure (a, o1', d1')
 
-shape :: Coding Cereal.PutM a -> PutM a
-shape m = PutM $ \(S o1 d1 o2 d2) -> case Cereal.getPutM (runCoding m go o2 d2) of
+shape :: Coding S.PutM a -> PutM a
+shape m = PutM $ \(S o1 d1 o2 d2) -> case S.getPutM (runCoding m go o2 d2) of
   ((a, o2', d2'), b) -> Result a (S o1 d1 o2' d2') (W b mempty mempty)
   where
-    go :: a -> Int -> Word8 -> Cereal.PutM (a, Int, Word8)
+    go :: a -> Int -> Word8 -> S.PutM (a, Int, Word8)
     go a o2' d2' = pure (a, o2', d2')
 
 -- should this log how much is put and just automatically scribble into shape?
 -- PutM doesn't give us enough info to do that efficiently.
-content :: Cereal.PutM a -> PutM a
-content m = PutM $ \s -> case Cereal.getPutM m of
+content :: S.PutM a -> PutM a
+content m = PutM $ \s -> case S.getPutM m of
   (a, b) -> Result a s (W mempty mempty b)
 
 {-
@@ -114,15 +122,64 @@ putPair l r (a,b) = putParens (putParens (l a) *> putParens (r b))
 put8 :: Word8 -> Put
 put8 w = meta (putLSB False) *> content (putWord8 w)
 
+putN :: Int -> S.Put -> Put
+-- TODO: replace that replicateM_ !!!
+putN i w = meta (replicateM_ i $ putLSB False) *> content w
+
+putN_ :: S.Put -> Put
+-- TODO: replace that replicateM_ !!!
+putN_ w = meta (replicateM_ n $ putLSB False) *> content (S.putByteString bs)
+  where 
+    bs = S.runPut w
+    n = Strict.length bs
+
+--------------------------------------------------------------------------------
+-- * Puttable
+--------------------------------------------------------------------------------
+
 class Puttable a where
   put :: a -> Put
   default put :: (G.Generic a, GPuttable (G.Rep a)) => a -> Put
   put = gput . G.from
 
-instance Puttable ()
+instance Puttable Word64 where
+  put = putN 8 . S.putWord64le
+
+instance Puttable Word32 where
+  put = putN 4 . S.putWord32le
+
+instance Puttable Word16 where
+  put = putN 2 . S.putWord16le
 
 instance Puttable Word8 where
-  put = put8
+  put = putN 1 . S.putWord8
+
+instance Puttable Int64 where
+  put = putN 8 . S.putInt64le
+
+instance Puttable Int32 where
+  put = putN 4 . S.putInt32le
+
+instance Puttable Int16 where
+  put = putN 2 . S.putInt16le
+
+instance Puttable Int8 where
+  put = putN 1 . S.putInt8
+
+instance Puttable Void
+instance Puttable ()
+instance Puttable (Proxy a)
+instance Puttable a => Puttable (Maybe a)
+instance Puttable a => Puttable [a]
+instance (Puttable a, Puttable b) => Puttable (a, b)
+instance (Puttable a, Puttable b) => Puttable (Either a b)
+instance Puttable (f (g a)) => Puttable (Compose f g a)
+instance (Puttable (f a), Puttable (g a)) => Puttable (F.Product f g a)
+instance (Puttable (f a), Puttable (g a)) => Puttable (F.Sum f g a)
+
+--------------------------------------------------------------------------------
+-- * GPuttable
+--------------------------------------------------------------------------------
 
 class GPuttable t where
   gput :: t a -> Put
@@ -149,10 +206,27 @@ instance (GPuttable f, GPuttable g) => GPuttable (f G.:+: g) where
 instance (Puttable1 f, GPuttable g) => GPuttable (f G.:.: g) where
   gput = put1 gput . G.unComp1
 
+--------------------------------------------------------------------------------
+-- * Puttable1
+--------------------------------------------------------------------------------
+
 class Puttable1 f where
   put1 :: (a -> Put) -> f a -> Put
   default put1 :: (G.Generic1 f, GPuttable1 (G.Rep1 f)) => (a -> Put) -> f a -> Put
   put1 f = gput1 f . G.from1
+
+instance Puttable1 Proxy
+instance Puttable1 Maybe
+instance Puttable1 []
+instance Puttable a => Puttable1 (Either a)
+instance Puttable a => Puttable1 ((,) a)
+instance (Functor f, Puttable1 f, Puttable1 g) => Puttable1 (Compose f g)
+instance (Puttable1 f, Puttable1 g) => Puttable1 (F.Product f g)
+instance (Puttable1 f, Puttable1 g) => Puttable1 (F.Sum f g)
+
+--------------------------------------------------------------------------------
+-- * GPuttable1
+--------------------------------------------------------------------------------
 
 class GPuttable1 f where 
   gput1 :: (a -> Put) -> f a -> Put
